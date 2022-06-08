513f3bf076e2661aebfef3c414baf3a19aeefd59
Adarsh Jaiswal <adajaiswal@twitter.com>
2022-06-07 22:09:52 -0400

    web app

---
+++ gs-spring-boot-main/complete/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,30 @@
+package com.example.springboot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import com.google.gson.Gson;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@PostMapping("/payload")
+	public String payload(@RequestBody Object obj) {
+
+		String jsonString = new Gson().toJson(obj, Map.class);
+		System.out.println(jsonString);
+		return "worked";
+	}
+
+}
